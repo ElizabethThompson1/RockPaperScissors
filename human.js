2da2fad6577972dde1caa1c6eadf4962572830ce
ElizabethThompson1 <usawsoccer11@yahoo.com>
2021-11-02 16:18:18 -0500

    added classes

---
+++ human.js
@@ -0,0 +1,14 @@
+"use strict"
+class People{
+    constructor(name){
+        this.name = name;
+        this.gesture = [];
+    }
+    addgesture(){
+        this.gesture.push* (" Scissors","Paper" ,"Rock" ,"Lizard" ,"Spock");
+    }
+
+}
+console.log (People)
+
+module.exports = People;
