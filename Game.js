2aea7f0c3aa6f93b54408c51eb8ce0d53a4d307f
ElizabethThompson1 <usawsoccer11@yahoo.com>
2021-11-04 11:56:23 -0500

    completed if statment and made minor corrections to other functions

--- Game.js
+++ Game.js
@@ -10,7 +10,8 @@ class Game {
     }
     
     PickWhoToPlay(){
-        let QuestionPickAplayer = prompt("Who would you like to play 1 or 2? 1.Human or 2.AI?")
+        console.log("Who would you like to play 1 or 2? 1.Human or 2.AI?")
+        let QuestionPickAplayer = prompt();
         if (QuestionPickAplayer === "1"){
             this.humanVsHuman()
         }
@@ -27,17 +28,99 @@ class Game {
         this.playerOne = new Human ("Player One")
         this.playerTwo = new Human ("Player One")
     } 
-    
-    choosesWhoWins(){
-        switch(this.gesture){
-            case "Rock"
-            
-        }
+    pointsForPlayer(){
+
     }
+    // ake a new function to add points where console.log is 
+    choosesWhoWins(){
+        if (this.gesture ==="Rock"){
+            if( this.gesture ==="Scissors"){
+                this.Player.score++
+            }
+            else if (this.gesture === "Lizard"){
+                this.Player.score++
+            }
+            else if (this.gesture === "Rock"){
+                console.log("it's  TIE!")
+            }
+            else {
+                console.log ("You lost this round.")
+            }
+        
+
+        }
+        else if (this.gesture === "Scissors" ){
+            if(this.gesture === "Paper"){
+                this.Player.score++
+            }
+            else if(this.gesture === "Lizard"){
+                this.Player.score++
+            }
+            else if (this.gesture === "Scissors"){
+                console.log("it's  TIE!")
+            }
+            else {
+                console.log ("You lost this round.")
+            }
+        
+        }
+        else if (htis.gesture === "Paper"){
+            if(this.gesture === "Rock"){
+                this.Player.score++
+            }
+            else if(this.gesture === "Spock"){
+                this.player.score++
+            }
+            else if (this.gesture === "Paper"){
+                console.log("it's  TIE!")
+            }
+            else {
+                console.log ("You lost this round.")
+            }
+        }
+        else if (this.gesture === "Lizard"){
+            if (this.gesture === "Spock"){
+                this.Player.score++
+            }
+            else if (this.gesture === "Paper"){
+                this.Player.score++
+            }
+            else if (this.gesture === "Lizard"){
+                console.log("it's  TIE!")
+            }
+            else {
+                console.log ("You lost this round.")
+            }
+        }
+        else if (this.gesture === "Spock"){
+            if (this.gesture === "Scissors"){
+                this.player.score++
+            }
+            else if (this.gesture === "Rock"){
+                this.player.score++
+            }
+            else if (this.gesture === "Spock"){
+                console.log ("It's a TIE!")
+            }
+            else {
+                console.log ("You lost this round.")
+            }
+
+
+        }
+        
+    }
+
+
+    
+        
+    
     // whos wins out of whta the choose out of gestures
    
     runGame(){
-        this.PickWhoToPlay();    }
+        this.PickWhoToPlay(); 
+        this.choosesWhoWins();
+    }
 }
 module.exports={
  Game:Game
